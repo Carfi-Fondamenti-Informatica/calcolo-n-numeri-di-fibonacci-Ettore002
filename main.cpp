176e7256a5b4686ab42e6ef69cc780210be38b73
Ettore002 <91873786+Ettore002@users.noreply.github.com>
2021-11-14 19:54:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n;
+   cin>>n;
+   if(n>=2){
+      int x=0,y=1,z;
+      for(int i;i<n;i++){
+         z=x+y;
+         x=y;
+         y=z;
+         cout<<x<<endl;
+      }
+   }else{
+         cout<<"errore"endl;
+      }
    return 0;
 }
